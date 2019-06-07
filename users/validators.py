08dc4090149f1e51b069bb7b9b533b3c3fa6a30a
Willy Njundong <njundongw@gmail.com>
2019-06-06 22:13:35 -0700

    fixed validator

--- users/validators.py
+++ users/validators.py
@@ -2,9 +2,9 @@ from django.core.exceptions import ValidationError
 from django.utils.translation import ugettext_lazy as _
 
 
-def validate_accepted(response):
+def validate_accepted(reexitsponse):
 
-    if response:
+    if response is False:
         raise ValidationError(
             _("You must agree in order to proceed")
         )
