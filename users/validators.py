4ceac7cf35f0a8dd9e4c08255adb4fddad91fe1f
Willy Njundong <njundongw@gmail.com>
2019-06-06 22:16:29 -0700

    fixed typo :/

--- users/validators.py
+++ users/validators.py
@@ -2,7 +2,7 @@ from django.core.exceptions import ValidationError
 from django.utils.translation import ugettext_lazy as _
 
 
-def validate_accepted(reexitsponse):
+def validate_accepted(response):
 
     if response is False:
         raise ValidationError(
