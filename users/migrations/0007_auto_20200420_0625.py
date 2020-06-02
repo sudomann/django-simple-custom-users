4c12cdc0cac863b8c6c85753e6c8cec6b41d79b7
Willy Njundong <njundongw@gmail.com>
2020-06-02 13:58:15 -0400

    made new migration and then squashed

--- users/migrations/0007_auto_20200420_0625.py
+++ users/migrations/0007_auto_20200420_0625.py
@@ -15,16 +15,19 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='customuser',
             name='privacy_policy_version',
-            field=models.ForeignKey(default=users.models.get_current_privacy, on_delete=django.db.models.deletion.PROTECT, to='users.PrivacyPolicy'),
+            field=models.ForeignKey(
+                on_delete=django.db.models.deletion.PROTECT, to='users.PrivacyPolicy'),
         ),
         migrations.AddField(
             model_name='customuser',
             name='security_policy_version',
-            field=models.ForeignKey(default=users.models.get_current_security, on_delete=django.db.models.deletion.PROTECT, to='users.SecurityPolicy'),
+            field=models.ForeignKey(
+                on_delete=django.db.models.deletion.PROTECT, to='users.SecurityPolicy'),
         ),
         migrations.AddField(
             model_name='customuser',
             name='terms_of_service_version',
-            field=models.ForeignKey(default=users.models.get_current_terms, on_delete=django.db.models.deletion.PROTECT, to='users.TermsOfService'),
+            field=models.ForeignKey(
+                on_delete=django.db.models.deletion.PROTECT, to='users.TermsOfService'),
         ),
     ]
