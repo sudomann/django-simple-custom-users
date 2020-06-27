a850c28c40eaad37b869f7712e49308f180048ca
Willy Njundong <njundongw@gmail.com>
2020-06-27 04:51:20 -0400

    defined deps where they belong

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     version='2.0.1',
     packages=find_packages(),
     include_package_data=True,
-    install_requires=['psycopg2==2.8.4', ],
+    install_requires=['psycopg2==2.8.4', 'django-countries==6.1.2', 'django-phonenumber-field==3.0.1', 'phonenumbers==8.10.13'],
     license='Apache 2.0 License',
     description='A simple Django app that provides a custom user model.',
     long_description=README,
